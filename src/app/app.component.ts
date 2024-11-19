4d8f5b78876f3191ff40e8988280c7454539e621
Mona Lisa <Mona Lisa>
2024-11-18 21:15:36 -0500

    cambios 19-11-24 / Modelos y componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PadreComponent } from "./padre/padre.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trinity';
+  title = 'Trinity_Juank';
+  subtitulo = 'El mejor'
+
+  metodos() {
+
+  }
 }
